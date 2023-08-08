ec3899155961e7e21c339f5f68e548a453777c56
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-08-08 16:45:19 +0900

    init: SpringBoot 서버 생성
    java 11
    SpringBoot 2.7.14
    MySQL
    Lombok
    SpringSecurity

---
+++ server/MZTI_server/src/test/java/com/example/mzti_server/MztiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mzti_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MztiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
