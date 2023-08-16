5cb9c2b4f845ec3f114686ea2f11c5b13422a991
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-08-16 19:53:52 +0900

    refactor: 특정 MBTI 검색 response type 조정

--- server/MZTI_server/src/main/java/com/example/mzti_server/dto/MBTI/MbtiResponseDTO.java
+++ server/MZTI_server/src/main/java/com/example/mzti_server/dto/MBTI/MbtiResponseDTO.java
@@ -2,35 +2,9 @@ package com.example.mzti_server.dto.MBTI;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
 import java.util.List;
 
 @Data
 public class MbtiResponseDTO {
     private final String category;
-    private final List<String> descriptions;
-    private final String goodJob; // 어울리는 일
-    private final String loveStyle; // 연애 스타일
-    private final String goodPeople; // 잘 맞는 사람
-    private final String goodMBTI; // 잘 맞는 MBTI
-    private final String badMBTI; // 잘 안 맞는 MBTI
-    private final String talkingHabit; // 말버릇
-    private final String keyword; // 키워드
-    private final String virtualPeople; // 가상 인물
-
-    @Builder
-    public MbtiResponseDTO(String category, List<String> descriptions, String goodJob, String loveStyle, String goodPeople, String goodMBTI, String badMBTI, String talkingHabit,
-                           String keyword, String virtualPeople){
-        this.category = category;
-        this.descriptions = descriptions;
-        this.goodJob = goodJob;
-        this.loveStyle = loveStyle;
-        this.goodPeople = goodPeople;
-        this.goodMBTI = goodMBTI;
-        this.badMBTI = badMBTI;
-        this.talkingHabit = talkingHabit;
-        this.keyword = keyword;
-        this.virtualPeople = virtualPeople;
-    }
 }
--- server/MZTI_server/src/main/java/com/example/mzti_server/service/InfoService.java
+++ server/MZTI_server/src/main/java/com/example/mzti_server/service/InfoService.java
@@ -2,17 +2,18 @@ package com.example.mzti_server.service;
 
 import com.example.mzti_server.domain.MbtiDescription;
 import com.example.mzti_server.domain.MbtiInfo;
+import com.example.mzti_server.dto.MBTI.MbtiDataDTO;
+import com.example.mzti_server.dto.MBTI.MbtiInfoDTO;
 import com.example.mzti_server.dto.MBTI.MbtiResponseDTO;
 import com.example.mzti_server.repository.MbtiDescriptionRepository;
 import com.example.mzti_server.repository.MbtiInfoRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -22,32 +23,48 @@ public class InfoService {
 
     // MBTI 1개 조회
     public ResponseEntity<LinkedHashMap<String, Object>> getMBTIInfo(String mbti) {
-        MbtiResponseDTO mbtiResponseDTO = getMbtiResponseDTO(mbti);
-        return getResponse(mbtiResponseDTO);
+        MbtiInfo mbtiInfo = mbtiInfoRepository.findByCategory(mbti);
+
+        List<MbtiDataDTO> temp = new ArrayList<>();
+
+        List<MbtiDescription> mbtiDescriptionList = mbtiDescriptionRepository.findByMbtiInfoId(mbtiInfo.getId());
+        String[] descriptions = new String[6];
+        for (int i = 0; i < mbtiDescriptionList.size(); i++) {
+            descriptions[i] = mbtiDescriptionList.get(i).getContent();
+        }
+        MbtiDataDTO descriptions1 = new MbtiDataDTO("descriptions", descriptions);
+        temp.add(descriptions1);
+
+        String goodMBTIs = mbtiInfo.getGoodMBTI();
+        String badMBTIs = mbtiInfo.getBadMBTI();
+        String goodPeoples = mbtiInfo.getGoodPeople();
+        String goodJobs = mbtiInfo.getGoodJob();
+        String keywords = mbtiInfo.getKeyword();
+        String loveStyles = mbtiInfo.getLoveStyle();
+        String talkingHabbits = mbtiInfo.getTalkingHabit();
+        String virtualPeoples = mbtiInfo.getVirtualPeople();
+
+        String[] keys = {"keyword", "goodJob", "loveStyle", "goodPeople", "goodMBTIs", "badMBTIs", "talkingHabbit", "virtualPeople"};
+        String[] contents = {keywords, goodJobs, loveStyles, goodPeoples, goodMBTIs, badMBTIs, talkingHabbits, virtualPeoples};
+
+        for (int i = 0; i < keys.length; i++) {
+            String[] values = contents[i].split(",");
+            MbtiDataDTO mbtiDataDTO = new MbtiDataDTO(keys[i], values);
+            temp.add(mbtiDataDTO);
+        }
+
+        MbtiInfoDTO mbtiInfoDTO = new MbtiInfoDTO(temp);
+
+        return getResponse(mbtiInfoDTO);
     }
 
 
     // MBTI 2개 비교
     public ResponseEntity<LinkedHashMap<String, Object>> getComparedMBTI(String leftMBTI, String rightMBTI) {
         List<MbtiResponseDTO> responseDTOList = new ArrayList<>();
-        responseDTOList.add(getMbtiResponseDTO(leftMBTI));
-        responseDTOList.add(getMbtiResponseDTO(rightMBTI));
         return getResponse(responseDTOList);
     }
 
-    private MbtiResponseDTO getMbtiResponseDTO(String mbti) {
-        MbtiInfo mbtiInfo = mbtiInfoRepository.findByCategory(mbti);
-        List<MbtiDescription> mbtiDescriptionList = mbtiDescriptionRepository.findByMbtiInfoId(mbtiInfo.getId());
-        List<String> descriptions = new ArrayList<>();
-        for(int i=0;i<mbtiDescriptionList.size();i++){
-            descriptions.add(mbtiDescriptionList.get(i).getContent());
-        }
-        MbtiResponseDTO mbtiResponseDTO = MbtiResponseDTO.builder().category(mbtiInfo.getCategory()).descriptions(descriptions).goodJob(mbtiInfo.getGoodJob())
-                .loveStyle(mbtiInfo.getLoveStyle()).goodPeople(mbtiInfo.getGoodPeople()).goodMBTI(mbtiInfo.getGoodMBTI())
-                .badMBTI(mbtiInfo.getBadMBTI()).talkingHabit(mbtiInfo.getTalkingHabit()).keyword(mbtiInfo.getKeyword()).virtualPeople(mbtiInfo.getVirtualPeople())
-                .build();
-        return mbtiResponseDTO;
-    }
 
     private static ResponseEntity<LinkedHashMap<String, Object>> getResponse(Object saveMember) {
         LinkedHashMap<String, Object> response = new LinkedHashMap<>();
